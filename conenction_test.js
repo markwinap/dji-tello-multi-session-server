9de4bef0dee6c2cee7c4980420b98f099b96b586
Marco Martinez <markwinap@gmail.com>
2020-08-21 23:02:03 -0500

    update from pi

--- conenction_test.js
+++ conenction_test.js
@@ -1,63 +1,45 @@
-//CONSOLE COLORS
-const colors = require('colors');
+const { exec } = require("child_process");
 
-//UPP
-const dgram = require('dgram');
-const server = dgram.createSocket('udp4'); // UDP SERVER IPv4 FOR SENDING COMMANDS AND RECEIVING COMMAND CONFIRMATION
-const status = dgram.createSocket('udp4'); // UDP SERVER IPv4 FOR RECEIVING STATUS
-const video = dgram.createSocket('udp4'); // UDP SERVER IPv4 FOR RECEIVING VIDEO RAW H264 ENCODED YUV420p
-const port = 8889; //TELLO PORT
-const port_status = 8890; //TELLO STATUS PORT
-const port_video = 11111; //TELLO VIDEO PORT
-//WS
-const WebSocket = require('ws'); //WEBSOCKET
-const port_websocket = 8080; //WEBSOCKET PORT
-//VARIABLES
-const tello_default = '192.168.10.2';
-let videoBuff = []; //VIDEO BUFFER
-let counter = 0; //COUNTER FOR VIDEO BUFFER FRAMES
-let temp_input = '';
 
-let int;
-async function main() {
-  int = setInterval(() => {
-    console.log(server);
-  }, 1000);
+function exectCommand(){
+  return new Promise((resolve, reject) => {
+    exec("iwgetid -r", (error, stdout, stderr) => {
+      if (error) {
+        reject(error)
+      }
+      if (stderr) {
+        reject(stderr)
+      }
+      resolve(stdout)
+  });
+  })
 }
-main();
 
-//UDP CLIENT SERVER
-server.on('error', (err) => {
-  console.log(`server error:\n${err.stack}`);
-  server.close();
-});
-server.on('message', (msg, rinfo) => {
-  //UNCOMNET FOR DEBUG
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-  nextCMD(rinfo.address); //Check if commands available
-});
-server.on('listening', () => {
-  let address = server.address();
-  //UNCOMNET FOR DEBUG
-  console.log(`UDP CMD RESPONSE SERVER - ${address.address}:${address.port}`);
-});
-server.bind(port);
-//UDP STATUS SERVER
-status.on('listening', function () {
-  let address = status.address();
-  //UNCOMNET FOR DEBUG
-  console.log(`UDP STATUS SERVER - ${address.address}:${address.port}`);
-});
-status.on('message', function (message, remote) {
-  //UNCOMNET FOR DEBUG
-  //console.log(`${remote.address}:${remote.port} - ${message}`);
-  let msg_obj = dataSplit(message.toString());
-  if (commands.hasOwnProperty(remote.address)) {
-    commands[remote.address]['status'] = msg_obj;
-  } else {
-    commands = Object.assign(commands, {
-      [remote.address]: { status: msg_obj },
-    });
+const telloSSID = 'TELLO-AB855A';
+const wifiInterval = 1000;//MS
+let wifi = false;
+
+
+async function main(){
+  const test = setInterval(async ()=> {
+    wifiStatus();
+
+  }, wifiInterval)
+}
+main()
+
+
+
+
+const wifiStatus = async () => {
+  try{
+    const a = await exectCommand()
+    const ssid = a.replace(/(\r\n|\n|\r)/gm,'');
+    if(ssid == telloSSID){
+      wifi = true;
+    }
   }
-});
-status.bind(port_status);
+  catch(e){
+    wifi = false;
+  }
+}
