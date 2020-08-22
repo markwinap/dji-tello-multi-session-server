d977f1ad24ed1c508ddd497a1e61635e896f0385
Marco Martinez <markwinap@gmail.com>
2020-08-21 23:57:09 -0500

    Udp WIFI auto Connect

--- conenction_test.js
+++ conenction_test.js
@@ -1,4 +1,37 @@
 const { exec } = require("child_process");
+//UPP
+const dgram = require('dgram');
+let mainSocket = dgram.createSocket('udp4'); // UDP SERVER IPv4 FOR SENDING COMMANDS AND RECEIVING COMMAND CONFIRMATION
+
+const port = 8889; //TELLO PORT
+const port_status = 8890; //TELLO STATUS PORT
+const port_video = 11111; //TELLO VIDEO PORT
+
+const telloSSID = 'TELLO-AB855A';
+const telloIP = '192.168.10.2';
+const wifiInterval = 1000;//MS
+let wifiStatus = false;//
+let udpStatus = false;
+
+
+//UDP CLIENT SERVER
+mainSocket.on('error', (err) => {
+  console.log(`ERROR :\n${err.stack}`);
+  udpStatus = false;
+  mainSocket.close();
+});
+mainSocket.on('message', (msg, rinfo) => {
+  //UNCOMNET FOR DEBUG
+  console.log(`mainSocket got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  nextCMD(rinfo.address); //Check if commands available
+});
+mainSocket.on('listening', () => {
+  let address = mainSocket.address();
+  //UNCOMNET FOR DEBUG
+  console.log(`UDP CMD RESPONSE SERVER - ${address.address}:${address.port}`);
+});
+
+
 
 
 function exectCommand(){
@@ -15,31 +48,54 @@ function exectCommand(){
   })
 }
 
-const telloSSID = 'TELLO-AB855A';
-const wifiInterval = 1000;//MS
-let wifi = false;
 
 
-async function main(){
-  const test = setInterval(async ()=> {
-    wifiStatus();
 
+const main = async ()=> {
+  const mainInt = setInterval(async ()=> {
+    
+    checkWIFI();
+    checkUDP()
   }, wifiInterval)
-}
+};
 main()
 
 
+const checkUDP  = async () => {
+  if(wifiStatus && !udpStatus){
+    console.log('CONNECT SERVER')
+    console.log(`WIFI ${wifiStatus} UDP ${udpStatus}`)
+    connectUDPServers()
 
+  }
+  if(!wifiStatus && udpStatus){
+    console.log('CLOSE SERVER')
+    console.log(`WIFI ${wifiStatus} UDP ${udpStatus}`)
+    disconnectUDPServers()
 
-const wifiStatus = async () => {
+  }
+}
+const checkWIFI = async () => {
   try{
     const a = await exectCommand()
     const ssid = a.replace(/(\r\n|\n|\r)/gm,'');
     if(ssid == telloSSID){
-      wifi = true;
+      wifiStatus = true;
     }
   }
   catch(e){
-    wifi = false;
+    wifiStatus = false;
   }
+}
+const connectUDPServers = () => {
+  mainSocket = dgram.createSocket('udp4');
+  udpStatus = true;
+  mainSocket.bind({
+    address: telloIP,
+    port: port
+  });
+}
+const disconnectUDPServers = () => {
+  udpStatus = false;
+  mainSocket.close();
 }
